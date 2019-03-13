01d55d1c60729d191f7bcb9b8e1d0fc87ed31ff6
gim_razer <rgm79ms@gmail.com>
2019-03-13 13:43:06 +0800

    Initial commit

---
+++ Script/QRCodeGenerator.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using UnityEngine;
+using ZXing;
+using ZXing.QrCode;
+using System;
+
+public class QRCodeGenerator : MonoBehaviour {
+
+    public int textureSize = 256;
+
+    public delegate void QRCodeGenerated(Texture2D _texture);
+    public static event QRCodeGenerated OnQRCodeGenerated;
+
+	void Start ()
+    {
+
+    }
+
+    public void GenerateQRCode(string _textToEncode)
+    {
+        StartCoroutine(E_GenerateQRCode(_textToEncode));
+    }
+
+    // TODO Fix the resolution problem
+    IEnumerator E_GenerateQRCode(string _textToEncode)
+    {
+        Texture2D texture_QRCode = new Texture2D(textureSize, textureSize);
+
+        texture_QRCode.SetPixels32(Encode(_textToEncode));
+
+        yield return null;
+
+        texture_QRCode.Apply();
+
+        if (OnQRCodeGenerated != null)
+            OnQRCodeGenerated(texture_QRCode);
+
+    }
+
+    Color32[] Encode(string _textToEncode)
+    {
+        BarcodeWriter barcodeWriter = new BarcodeWriter
+        {
+            Format = BarcodeFormat.QR_CODE,
+            Options = new QrCodeEncodingOptions
+            {
+                Height = textureSize,
+                Width = textureSize
+            }
+        };
+
+        return barcodeWriter.Write(_textToEncode);
+    }
+
+    /*
+    public void SaveQRCodeFile(Texture2D _qrCodeTex)
+    {
+
+        try
+        {
+            string fileName = gameManager.PhotoFileName + "_QR.png";
+            string fullPath = gameManager.qrCodeSavePath + "\\" + fileName;
+
+            byte[] bytes = _qrCodeTex.EncodeToPNG();
+
+            System.IO.File.WriteAllBytes(fullPath, bytes);
+
+            //Debug.Log("QRCode File Saved: " + fullPath);
+        }
+        catch(Exception e)
+        {
+            Debug.LogError(e.Message);
+        }
+       
+    }
+    */
+}
---
+++ Script/WebTool.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+using LitJson;
+using UnityEngine.UI;
+
+public class WebTool : MonoBehaviour
+{
+    public string urlForUploading = "http://139.162.14.162:3027/api/fileTrans/upload";
+
+    public string bodyKey = "upload";
+    public string mimeType = "image/png";
+    public string myUrlPrefix = "http://139.162.14.162:3027";
+    JsonData jsonData;
+
+    public bool UploadSuccess { get { return uploadSuccess; } }
+    bool uploadSuccess;
+
+    string photoUrl;
+
+    // An event which will be triggered when the uploading request is done
+    public delegate void UploadPhotoReqFinish(bool success, string photoUrl);
+    public static event UploadPhotoReqFinish OnUploadPhotoReqFinish;
+
+    void Start()
+    {
+
+    }
+
+    public void UploadPhoto(Texture2D _textureToUpload, string _fileName)
+    {
+        StartCoroutine(E_UploadPhoto(_textureToUpload, _fileName));
+    }
+
+    IEnumerator E_UploadPhoto(Texture2D _textureToUpload, string _fileName)
+    {
+        byte[] bytes = _textureToUpload.EncodeToPNG();
+
+        WWWForm form = new WWWForm();
+        form.AddBinaryData(bodyKey, bytes, _fileName, mimeType);
+
+        using (UnityWebRequest req = UnityWebRequest.Post(urlForUploading, form))
+        {
+            yield return req.SendWebRequest();
+
+            // Status Code
+            Debug.Log("WebRequest Status Code: " + req.responseCode);
+
+            if (req.isNetworkError || req.isHttpError)
+            {
+                uploadSuccess = false;
+                Debug.LogError(req.error);
+            }
+            else
+            {
+                Debug.Log("Uploaded!");
+
+                // Get the downloaded json string
+                string downloadedString = req.downloadHandler.text;
+                Debug.Log(downloadedString);
+
+                // Parse the downloaded json string and get the url
+                string parsedUrl = GetPhotoUrl(downloadedString);
+                if (parsedUrl != null)
+                {
+                    uploadSuccess = true;
+
+                    //photoUrl = myUrlPrefix + parsedUrl;
+                    photoUrl = parsedUrl;
+                    Debug.Log(photoUrl);
+                }
+                else
+                {
+                    uploadSuccess = false;
+                    Debug.LogError("Something's wrong when parsing the json and get the url.");
+                }
+            }
+
+            if (OnUploadPhotoReqFinish != null)
+                OnUploadPhotoReqFinish(uploadSuccess, photoUrl);
+        }
+    }
+
+    string GetPhotoUrl(string _jsonString)
+    {
+        jsonData = JsonMapper.ToObject(_jsonString);
+
+        if (jsonData["url"] != null)
+        {
+            return jsonData["url"].ToString();
+        }
+        else
+        {
+            Debug.LogError("jsonData[url] is null!");
+        }
+
+        return null;
+    }
+}
